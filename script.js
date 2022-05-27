0c353c258ab4dd12787068024338d01930fcc048
Andrew <abysmalduck@gmail.com>
2022-05-27 12:23:10 -0700

    Add type to other pieces

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-/* Create Types here */
-type PlayerID = 1 | 2;
-type Space = 'X' | 'O' | ' ';
-type PlayerSymbol = 'X' | 'O';
-
-/* 
+"use strict";
+/*
 Set event listener for the Game Board
 */
 const gameBoard = document.querySelector('#gameboard');
-gameBoard!.addEventListener('click', function(event: Event) {
+gameBoard.addEventListener('click', function (event) {
     // Determine where the click was based on if statements
     const clicked = event.target;
     let _player = Game.whoseTurn();
@@ -15,39 +11,28 @@ gameBoard!.addEventListener('click', function(event: Event) {
     console.log(clicked);
     console.log(clicked.id);
 });
-
-
-
 const GameBoard = (function () {
-    /* 
+    /*
     Object for containing the game board itself
     Game board will consist of an array to represent the spaces on the board
     */
-
-    let _GBArray: Space[] = []; // Gameboard Array
-
+    let _GBArray = []; // Gameboard Array
     const setBoard = function () {
         const gameBoard = document.getElementById('gameboard');
-
         //Create the gameboard
         for (let i = 0; i > 8; i++) {
             _GBArray[i] = ' ';
         }
         // displayController.refresh();
-
-
-    }
-
-    const updateBoard = function(player, square) {
+    };
+    const updateBoard = function (player, square) {
         // Validate it is the player's turn
-
         // Update board with player move
         // Inputs are player (X/O) and square (1-9) (index position 0-8)
         // Square value will be updated with player symbol
         // Publish game board update
-    }
-
-    const checkWinner = function() {
+    };
+    const checkWinner = function () {
         //Series of if statements to determine
         if ((_GBArray[0] === _GBArray[1] && _GBArray[1] === _GBArray[2]) ||
             (_GBArray[3] === _GBArray[4] && _GBArray[4] === _GBArray[5]) ||
@@ -60,47 +45,28 @@ const GameBoard = (function () {
         ) {
             // Publish game winner
         }
-    }
-
-    return {updateBoard, setBoard};
+    };
+    return { updateBoard, setBoard };
 })();
-
 const Game = (function () {
     /* Module for containing game logic */
-
     let _playing = false; // Switch for game on
-    let _playerTurn: PlayerID;
-
-    const startGame = function() {
+    let _playerTurn;
+    const startGame = function () {
         // Let player 1 choose a symbol
         // Assign player 2 the other
         // Set the turn to player 1
         // Set up the game board
         // Set _playing variable to true
-    }
-
+    };
     const nextTurn = function () {
         // set _playerTurn to next
-    }
-
-    const playTurn = function (player: typeof Player, space: Space) {
-
-    }
-
+    };
+    const playTurn = function (player, space) {
+    };
     const whoseTurn = function () {
         //Get whose turn it is
         return _playerTurn;
-    }
-
-    return {whoseTurn, playTurn};
-
+    };
+    return { whoseTurn, playTurn };
 })();
-
-const Player = function (id: PlayerID, symbol: PlayerSymbol) {
-
-
-    return {
-        id,
-        symbol
-    }
-}
